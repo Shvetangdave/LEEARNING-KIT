7995317ea7afd85d5bb2b8ea73b8f34bf0c91c5e
Devarsh <87245954+Shvetangdave@users.noreply.github.com>
2022-03-22 19:09:52 -0400

    FILE UPLOAD

---
+++ fillinguwc.js
@@ -0,0 +1,155 @@
+
+
+
+function red() {
+    document.body.style.backgroundColor = "red";
+ speakred();
+  
+}
+function speakred(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Red";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function Blue() {
+    document.body.style.backgroundColor = "blue";
+ 
+  speakblue();
+}
+
+function speakblue(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Blue";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+
+
+
+
+
+
+function white() {
+    document.body.style.backgroundColor = "white";
+ 
+  speakwhite();
+}
+function speakwhite(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="White";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function pink() {
+    document.body.style.backgroundColor = "pink";
+ 
+  speakpink();
+}
+function speakpink(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Pink";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function black() {
+    document.body.style.backgroundColor = "black";
+ 
+  speakblack();
+}
+function speakblack(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Black";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function orange() {
+    document.body.style.backgroundColor = "orange";
+ 
+  speakorange();
+}
+function speakorange(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Orange";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function brown() {
+    document.body.style.backgroundColor = "brown";
+ 
+  speakbrown();
+}
+function speakbrown(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Brown";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function grey() {
+    document.body.style.backgroundColor = "grey";
+ speakgrey();
+  
+}
+function speakgrey(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Grey";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+ 
+  
+    speakgreen();
+  
+}
+function speakgreen(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Green";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+function yellow() {
+    document.body.style.backgroundColor = "yellow";
+ 
+  
+    speakyellow();
+  
+}
+function speakyellow(){
+        
+    
+    var synth=window.speechSynthesis;
+    speak_data="Yellow";
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+function back(){
+    window.location="Intro.html"
+}
